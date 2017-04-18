a578c756c7a8a470b4897af8450271ac5345e21b
jas <jasbholla1990@gmail.com>
2017-04-18 01:03:37 -0700

    Implementation of Singly Linked List

---
+++ python/singly_linked_list.py
@@ -0,0 +1,122 @@
+class SinglyLinkedList(object):
+    """docstring for SinglyLinkedList"""
+    def __init__(self, name="Test"):
+        super(SinglyLinkedList, self).__init__()
+        self.name = name
+        self.head = None
+        self.tail = None
+
+    def insertNode(self, value):
+        if self.head is None:
+            self.head = self.tail = Node(value, None)
+        else:
+            node = self.getLastNode()
+            node.next = Node(value, None)
+
+    def insertNodeAtIndex(self, index, value):
+        if index == 0 and self.head == None:
+            self.insertNode(value)
+        elif index == 0:
+            newNode = Node(value, self.head)
+            self.head = newNode
+        else:
+            node = self.getNodeAtIndex(index-1)
+
+            if node.element == None:
+                return
+            else:
+                newNode = Node(value, node.next)
+                node.next = newNode
+
+    def getNodeAtIndex(self, index):
+        node = self.head
+        currentIndex = 0
+
+        while hasattr(node, 'element') and currentIndex != index:
+            currentIndex += 1
+
+            if node.next != None:
+                node = node.next
+            else:
+                node = Node(None, None)
+
+        if hasattr(node, 'element'):
+            return node
+        else:
+            return Node(None, None)
+
+    def getLastNode(self):
+        node = self.head
+
+        while hasattr(node, 'element'):
+            if node.next != None :
+                node = node.next
+            else:
+                return node
+
+        if node == None:
+            node = Node(None, None)
+
+        return node
+
+
+    def deleteAllNodes(self):
+        self.head = None
+
+    def deleteNodeAtIndex(self, index):
+        if index == 0:
+            self.head = self.head.next if hasattr(self.head, 'next') else None
+        else:
+            nodeBefore = self.getNodeAtIndex(index-1)
+            if nodeBefore.element == None or nodeBefore.next == None:
+                return
+            else:
+                nodeToDelete = nodeBefore.next
+                if nodeToDelete.next != None:
+                    nodeBefore.next = None
+                else:
+
+
+
+    def getAllNodes(self):
+        nodes = []
+        node = self.head
+
+        while hasattr(node, 'element'):
+            nodes.append(node.element)
+            node = node.next
+
+        return nodes
+
+    def getFirstNode(self):
+        node = self.head
+
+        if node == None:
+            node = Node(None, None)
+
+    def numberOfNodes(self):
+        node = self.head
+        numberOfNodes = 0
+
+        while hasattr(node, 'element'):
+            node = node.next
+            numberOfNodes += 1
+
+        return numberOfNodes
+
+    def printAllValues(self):
+        node = self.head
+
+        while hasattr(node, 'element'):
+            print node.element
+            node = node.next
+
+class Node(object):
+    """docstring for Node"""
+    def __init__(self, value, nextNode):
+        super(Node, self).__init__()
+        self.element = value
+        self.next = nextNode
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ python/test.py
@@ -0,0 +1,81 @@
+import unittest
+
+import singly_linked_list as linkedList
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        print "In method", self._testMethodName
+
+    def test_Created_LinkedList_NodesNumber(self):
+        llist = linkedList.SinglyLinkedList()
+        self.assertEqual(llist.numberOfNodes(),0)
+
+        llist.insertNode(32)
+        llist.insertNode(132)
+        llist.insertNode(4)
+        self.assertEqual(llist.numberOfNodes(), 3)
+
+        llist.deleteNodeAtIndex(0)
+        self.assertEqual(llist.numberOfNodes(), 2)
+
+        llist.deleteAllNodes()
+        self.assertEqual(llist.numberOfNodes(), 0)
+
+    def test_Create_Add_Delete_Nodes(self):
+        llist = linkedList.SinglyLinkedList()
+
+        llist.insertNodeAtIndex(0,1)
+        self.assertEqual(llist.getAllNodes(), [1])
+
+        llist.deleteAllNodes()
+
+        llist.insertNode(32)
+        llist.insertNode(132)
+        llist.insertNode(4)
+        self.assertEqual(llist.getAllNodes(), [32,132,4])
+
+        llist.insertNodeAtIndex(1,11)
+        self.assertEqual(llist.getAllNodes(), [32,11,132,4])
+
+        llist.insertNodeAtIndex(10,99)
+        self.assertEqual(llist.getAllNodes(), [32,11,132,4])
+
+        llist.insertNodeAtIndex(2,10101)
+        self.assertEqual(llist.getAllNodes(), [32,11,10101,132,4])
+
+        llist.insertNodeAtIndex(0,9009)
+        self.assertEqual(llist.getAllNodes(), [9009,32,11,10101,132,4])
+
+        # llist.insertNodeAtIndex(0,1010)
+        # self.assertEqual(llist.getAllNodes(),[1010,32,11,132,4,99])
+
+    def test_GetNode_From_Particular_Index(self):
+        llist = linkedList.SinglyLinkedList()
+
+        llist.insertNode(32)
+        llist.insertNode(132)
+        llist.insertNode(4)
+        self.assertEqual(llist.getNodeAtIndex(0).element, 32)
+
+        self.assertEqual(llist.getNodeAtIndex(1).element, 132)
+
+        self.assertEqual(llist.getNodeAtIndex(2).element, 4)
+        self.assertEqual(llist.getNodeAtIndex(2).next, None)
+
+        self.assertEqual(llist.getNodeAtIndex(4).element, None)
+        self.assertEqual(llist.getNodeAtIndex(4).next, None)
+
+    def test_DeleteNode_From_Particular_Index(self):
+        llist = linkedList.SinglyLinkedList()
+
+        llist.insertNode(32)
+        llist.insertNode(132)
+        llist.insertNode(4)
+        llist.deleteNodeAtIndex(1)
+
+        self.assertEqual(llist.getAllNodes(), [32,4])
+
+
+if __name__ == '__main__':
+    unittest.main()
